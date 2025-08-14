1621691d771fdb8c8f55d06102f40ddc13bff733
QKhangGyn <khangnguyen3111999@gmail.com>
2025-08-14 15:48:36 +0700

    implementation 1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,8 +49,9 @@ then
 	fi
 fi
 #echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# Remove make steps according to assignment3 par1 implementation 1
+#make clean
+#make
 
 for i in $( seq 1 $NUMFILES)
 do
